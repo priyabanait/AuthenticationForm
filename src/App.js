d0b3ab6ddb96a04c8bc9999517ec1f760088d8a2
priyabanait <priyabanait91@gmail.com>
2023-08-08 15:40:49 +0530

    Addressing security and logout issues

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import { Routes, Route } from 'react-router-dom';
-
+import { Routes, Route,Navigate } from 'react-router-dom';
+import { useContext } from 'react';
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
-
+import AuthContext from './store/AuthContext';
 function App() {
+  const context=useContext(AuthContext);
   return (
     <Layout>
       <Routes>
         <Route path='/' Component={HomePage} exact>
-          
         </Route>
+
+      {!context.isLogin && (
         <Route path='/auth' Component={AuthPage}>
-          
-        </Route>
-        <Route path='/profile' Component={UserProfile}>
-         
-        </Route>
+        </Route>)}
+
+        {context.isLogin &&<Route path='/profile' Component={UserProfile}>
+        </Route>}
+        
+         <Route path="*" element={<Navigate replace to="/auth" />} />
       </Routes>
     </Layout>
   );
